9aef7c2744ba87631aa6bfc3c4f9fff87d4696d7
Zackaria <scehr@outlook.com>
2023-04-15 00:03:13 +0100

    Smoothed movement with SmoothDamp. Looking at smoothed turning.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,13 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private LayerMask environmentMask;
+    [SerializeField]
+    private float moveSpeed = 0.0f;
+
+    private Vector2 currentInputVector;
+    private Vector2 smoothInputVelocity;
+    [SerializeField]
+    private float smoothInputSpeed;
+
+    [SerializeField, Tooltip("Mask for look raycast to hit.")]
+    private LayerMask environmentMask;
+    
     private Vector3 lookPos;
     private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
     private Rigidbody playerRigidbody;
 
-    public float moveSpeed = 1f;
+
 
     private void Awake()
     {
@@ -33,8 +43,8 @@ public class PlayerMovement : MonoBehaviour
     private void LookAtMouse()
     {
         Vector2 mouseVector = playerInputActions.Player.Look.ReadValue<Vector2>();
-
         Ray ray = Camera.main.ScreenPointToRay(mouseVector);
+
         if (Physics.Raycast(ray, out var hit, 100, environmentMask))
         {
             lookPos = hit.point;
@@ -47,7 +57,9 @@ public class PlayerMovement : MonoBehaviour
     private void Move()
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
-        Vector3 moveVector = new Vector3(inputVector.x, 0, inputVector.y);
+        currentInputVector = Vector2.SmoothDamp(currentInputVector, inputVector, ref smoothInputVelocity, smoothInputSpeed);
+        Vector3 moveVector = new Vector3(currentInputVector.x, 0, currentInputVector.y);
+
         playerRigidbody.MovePosition(transform.position + moveVector * moveSpeed * Time.fixedDeltaTime);
     }
 }
